4dcadb2c9927bbce0df8e459f7b9334082104ed8
jcherqui <joachim.cherqui@viacesi.fr>
2016-05-27 11:29:01 +0200

    Add curses library & key events

--- apple.py
+++ apple.py
@@ -1,15 +1,19 @@
 from random import randint
 
 class Apple(object):
-    def __init__(self, grid):
-        self.position = [2, 8]
-        self.grid = grid
+    def __init__(self, window):
+        self.position = [2, 35]
+        self.window = window
 
     def display(self):
-        row, column = self.position
-        self.grid.grid[row][column] = 'a'
+        line, column = self.position
+        self.window.addstr(line, column, 'a')
+
+    def random(self):
+        height, width = self.window.getmaxyx()
+        line = randint(1, height - 2)
+        column = randint(1, width - 2)
+        self.position = [line, column]
 
     def reset(self):
-        row = randint(1, self.grid.height - 2)
-        column = randint(1, self.grid.width - 2)
-        self.position = [row, column]
+        self.__init__(self.window)
--- snake.py
+++ snake.py
@@ -1,33 +1,40 @@
+from curses import KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT
+
 class Snake(object):
-    def __init__(self, grid):
+    def __init__(self, window):
         self.position = [[2,1], [2,2], [2,3], [2,4]]
         self.head = self.position[len(self.position) -1]
-        self.grid = grid
+        self.window = window
         self.is_eating = False
+        self.direction = KEY_RIGHT
 
     def display(self):
-        for row,column in self.position:
-            self.grid.grid[row][column] = 's'
+        for line,column in self.position:
+            self.window.addstr(line, column, 's')
 
     def move(self, direction):
-        head_row, head_column = self.head
+        self.direction = direction
+        head_line, head_column = self.head
 
         if not self.is_eating:
             self.position.pop(0)
 
         self.is_eating = False
 
-        if direction == 'up':
-            self.position.append([head_row - 1, head_column])
-        elif direction == 'down':
-            self.position.append([head_row + 1, head_column])
-        elif direction == 'left':
-            self.position.append([head_row, head_column - 1])
-        elif direction == 'right':
-            self.position.append([head_row, head_column + 1])
+        if direction == KEY_UP:
+            self.position.append([head_line - 1, head_column])
+        elif direction == KEY_DOWN:
+            self.position.append([head_line + 1, head_column])
+        elif direction == KEY_LEFT:
+            self.position.append([head_line, head_column - 1])
+        elif direction == KEY_RIGHT:
+            self.position.append([head_line, head_column + 1])
 
         self.head = self.position[len(self.position) -1]
 
     def eat(self, apple):
         self.is_eating = True
-        apple.reset()
+        apple.random()
+
+    def reset(self):
+        self.__init__(self.window)
